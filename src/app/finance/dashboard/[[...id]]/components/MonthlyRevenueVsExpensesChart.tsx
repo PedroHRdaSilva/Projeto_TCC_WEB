1c32b3ecf9b66ca1a90fe56204c51c2341d42dc1
Pedro <pedroribeiro.rds@gmail.com>
2025-11-19 21:06:51 -0300

    refactor-chart

--- src/app/finance/dashboard/[[...id]]/components/MonthlyRevenueVsExpensesChart.tsx
+++ src/app/finance/dashboard/[[...id]]/components/MonthlyRevenueVsExpensesChart.tsx
@@ -4,14 +4,7 @@ import { lightFormat } from "date-fns";
 import { format } from "date-fns/format";
 import { ptBR } from "date-fns/locale";
 import { parseISO } from "date-fns/parseISO";
-import {
-  Bar,
-  BarChart,
-  CartesianGrid,
-  ReferenceLine,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 import { useMonthlyRevenueVsExpensesQuery } from "@/graphql/hooks/graphqlHooks";
 import {
@@ -31,17 +24,18 @@ import type {
 import { Skeleton } from "@/lib/ui/skeleton";
 import { compactFormatter, formatCurrency } from "@/lib/utils/formatters";
 
-export const description = "A stacked bar chart with a legend";
+export const description = "A simple bar chart with grouped bars";
 const chartConfig = {
   revenue: {
     label: "Receitas",
     color: "var(--chart-2)",
   },
   expense: {
-    label: "Gasto",
+    label: "Gastos",
     color: "var(--chart-1)",
   },
 } satisfies ChartConfig;
+
 interface MonthlyRevenueVsExpensesChartProps {
   groupId: string;
   filterByStartMonth?: Date | null;
@@ -66,18 +60,11 @@ export function MonthlyRevenueVsExpensesChart({
   });
 
   const chartData =
-    data?.monthlyRevenueVsExpenses.map((item) => {
-      return {
-        time: format(parseISO(item.reportDate), "MMM/yyyy", { locale: ptBR }),
-        revenue: item.revenue,
-        expense: item.expense * -1,
-      };
-    }) ?? [];
-
-  const allValues = chartData.flatMap((d) => [d.revenue, d.expense]);
-  const maxAbsolute = allValues.reduce((acc, val) => {
-    return Math.abs(val) > Math.abs(acc) ? val : acc;
-  }, 0);
+    data?.monthlyRevenueVsExpenses.map((item) => ({
+      time: format(parseISO(item.reportDate), "MMM/yyyy", { locale: ptBR }),
+      revenue: item.revenue,
+      expense: item.expense,
+    })) ?? [];
 
   if (loading) {
     return (
@@ -88,6 +75,7 @@ export function MonthlyRevenueVsExpensesChart({
       </div>
     );
   }
+
   if (chartData.length === 0) {
     return (
       <Card className="flex h-[350px] items-center justify-center text-center">
@@ -113,6 +101,7 @@ export function MonthlyRevenueVsExpensesChart({
       </Card>
     );
   }
+
   return (
     <Card className="h-full">
       <CardHeader>
@@ -121,13 +110,13 @@ export function MonthlyRevenueVsExpensesChart({
           Fluxo de caixa ao longo do período
         </CardDescription>
       </CardHeader>
+
       <CardContent className="xl:p-0 xl:pl-1 xl:pr-4">
         <ChartContainer config={chartConfig} className="h-[220px] w-full">
-          <BarChart data={chartData} stackOffset="sign">
+          <BarChart data={chartData}>
             <CartesianGrid horizontal={false} />
             <XAxis
               dataKey="time"
-              type="category"
               tickLine={false}
               tickMargin={10}
               axisLine={false}
@@ -140,35 +129,23 @@ export function MonthlyRevenueVsExpensesChart({
               }
             />
 
-            <ReferenceLine
-              y={maxAbsolute}
-              stroke="gray"
-              strokeDasharray="3 3"
-            />
-            <ReferenceLine y={0} stroke="gray" strokeDasharray="3 3" />
-            <ReferenceLine
-              y={maxAbsolute * -1}
-              stroke="gray"
-              strokeDasharray="3 3"
-            />
             <ChartTooltip cursor={false} content={CustomTooltip} />
 
+            {/* Agora barras lado a lado */}
             <Bar
               dataKey="revenue"
               fill="var(--chart-green-dark)"
-              radius={[4, 4, 0, 0]}
               stroke="var(--chart-green-dark)"
               strokeWidth={2}
-              stackId="stack1"
+              radius={[4, 4, 0, 0]}
             />
 
             <Bar
               dataKey="expense"
               fill="var(--chart-5)"
-              radius={[4, 4, 0, 0]}
               stroke="var(--chart-5)"
               strokeWidth={2}
-              stackId="stack1"
+              radius={[4, 4, 0, 0]}
             />
           </BarChart>
         </ChartContainer>
@@ -176,30 +153,34 @@ export function MonthlyRevenueVsExpensesChart({
     </Card>
   );
 }
+
 function CustomTooltip({
   active,
   payload,
   label,
 }: TooltipContentProps<ValueType, NameType>) {
   if (!active || !payload || payload.length === 0) return null;
+
   const formattedLabel = label
     ?.toString()
     .replace(/^./, (c) => c.toUpperCase());
+
   return (
-    <div className="flex flex-col rounded bg-secondary p-2 text-sm shadow-md">
-      <p className="font-dm-sans text-[10px]">{formattedLabel}</p>
+    <div className="flex flex-col rounded bg-secondary p-2 text-lg shadow-md">
+      <p className="font-dm-sans text-lg">{formattedLabel}</p>
       {payload.map((item, idx) => (
-        <div key={idx} className="flex items-center gap-2 pt-2 text-xs">
+        <div key={idx} className="flex items-center gap-2 pt-2 text-lg">
           <span
             className="h-2 w-2 rounded-sm"
             style={{ backgroundColor: item.color }}
           ></span>
+
           <div className="flex w-full justify-between space-x-3">
-            <span className="font-dm-sans text-[9px] text-muted-foreground">
+            <span className="font-dm-sans  text-muted-foreground">
               {chartConfig[item.dataKey as keyof typeof chartConfig]?.label}:
             </span>
-            <span className="font-dm-sans text-[9px] text-muted-foreground">
-              {formatCurrency(Math.abs(item.value as number))}
+            <span className="font-dm-sans  text-muted-foreground">
+              {formatCurrency(item.value as number)}
             </span>
           </div>
         </div>
