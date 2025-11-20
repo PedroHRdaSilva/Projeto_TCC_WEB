a5c4ffb541c77afe435cf53aa6b556371d996ba9
Emanuel Costa <enmc0104@gmail.com>
2025-11-20 16:04:47 -0300

    Estilizando landing page

--- src/app/landing/header.tsx
+++ src/app/landing/header.tsx
@@ -19,15 +19,20 @@ export default function Header() {
     <header className="flex justify-between items-center p-4 bg-gray-100 dark:bg-gray-800">
       <Image src="/LogoDark.svg" alt="Logo" width={200} height={40} />
 
-      <div className="space-x-2">
+      <div className="space-x-2 pr-4">
         <Button
           variant="outline"
           onClick={handleLogin}
-          className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full"
+          className="bg-blue-500 hover:text-white hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full"
         >
           Entrar
         </Button>
-        <Button onClick={handleSignup}>Cadastre-se</Button>
+        <Button
+          onClick={handleSignup}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full"
+        >
+          Cadastre-se
+        </Button>
       </div>
     </header>
   );
